851ed9deb314a5569b94a2adefa7f7f13c6a160e
Brandon Johnson <brj.webside@gmail.com>
2021-05-06 20:29:15 -0500

    setting up the initial framework for the project

---
+++ calculator.js
@@ -0,0 +1,55 @@
+// need to make buttons correlating to each of the fucnctions 
+let input = ''; 
+
+let storedValue = '';
+
+
+function zeroInput() {
+ input += '0';
+}
+
+function oneInput() {
+ input += '1';
+}
+
+function twoInput() {
+ input += '2';
+}
+
+function threeInput() {
+ input += '3';   
+}
+
+function fourInput(){
+ input += '4';
+}
+
+function fiveInput() { 
+ input += '5';
+}
+
+function sixInput() {
+ input += '6';
+}
+
+function sevenInput() {
+ input += '7';
+}
+
+function eightInput() {
+ input += '8';
+}
+
+function nineInput() {
+ input += '9';
+}
+
+function add() {
+
+}
+ 
+function subtract()
+
+function multiply()
+
+function divide()
